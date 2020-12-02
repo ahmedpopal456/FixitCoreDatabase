56bdf4afc1c18b2f5db2b0755fb88aaa5c3249c8
Joanna Lin <33985435+jayohanna@users.noreply.github.com>
2020-12-01 23:38:03 -0500

    (#185) Implemented Dispose method

--- Fixit.Core.Storage/Fixit.Storage/Adapters/Cosmos/Internal/CosmosDatabaseAdapter.cs
+++ Fixit.Core.Storage/Fixit.Storage/Adapters/Cosmos/Internal/CosmosDatabaseAdapter.cs
@@ -8,11 +8,14 @@ namespace Fixit.Storage.Adapters.Cosmos.Internal
   internal class CosmosDatabaseAdapter : IDatabaseAdapter
   {
     private CosmosClient _cosmosClient;
+    private bool _disposed = false;
 
     public CosmosDatabaseAdapter(CosmosClient cosmosClient)
     {
       _cosmosClient = cosmosClient ?? throw new ArgumentNullException($"{nameof(CosmosDatabaseAdapter)} expects a value for {nameof(cosmosClient)}... null argument was provided");
     }
+
+    ~CosmosDatabaseAdapter() => Dispose(false);
 
     public async Task<IDatabaseTableAdapter> CreateDatabaseIfNotExistsAsync(string databaseId, CancellationToken cancellationToken)
     {
@@ -21,7 +24,23 @@ namespace Fixit.Storage.Adapters.Cosmos.Internal
 
     public void Dispose()
     {
-      _cosmosClient.Dispose();
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (_disposed)
+      {
+        return;
+      }
+
+      if (disposing)
+      {
+        _cosmosClient.Dispose();
+      }
+
+      _disposed = true;
     }
 
     public IDatabaseTableAdapter GetDatabase(string databaseId)
