f59d12ed5bc280d313288d04fda1891ffdda8c32
Joanna Lin <33985435+jayohanna@users.noreply.github.com>
2020-12-08 22:16:24 -0500

    (#294) Added unit tests for Cosmos mediators

---
+++ Fixit.Core.Database.UnitTests/FakeDataProviders/IFakeSeederFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fixit.Core.Database.UnitTests.Adapters
+{
+  public interface IFakeSeederFactory
+  {
+    IFakeSeederAdapter<T> CreateFakeSeeder<T>() where T : class;
+  }
+}
---
+++ Fixit.Core.Database.UnitTests/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Fixit.Core.Database.UnitTests.Adapters;
+using Fixit.Core.Database.Adapters;
+using Fixit.Core.Database.Mediators;
+
+namespace Fixit.Core.Database.UnitTests
+{
+  public class TestBase
+  {
+    public IFakeSeederFactory fakeDtoSeederFactory;
+
+    protected Mock<IDatabaseAdapter> _cosmosDatabaseAdapter;
+    protected Mock<IDatabaseTableAdapter> _cosmosDatabaseTableAdapter;
+    protected Mock<IDatabaseTableEntityAdapter> _cosmosDatabaseTableEntityAdapter;
+
+    public TestBase()
+    {
+      fakeDtoSeederFactory = new FakeDtoSeederFactory();
+    }
+
+    [AssemblyInitialize]
+    public static void AssemblyInitialize(TestContext testContext) { }
+
+    [AssemblyCleanup]
+    public static void AfterSuiteTests() { }
+  }
+}
