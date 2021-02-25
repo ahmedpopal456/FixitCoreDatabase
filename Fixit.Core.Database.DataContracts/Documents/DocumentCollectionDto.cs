a54a66ff5cfea2f5ac8e2095328f734c0630ad5e
a-paggabao <a.paggabao@hotmail.com>
2021-02-25 17:17:49 -0500

    #366 Converted dtos to DataContracts

--- Fixit.Core.Database.DataContracts/Documents/DocumentCollectionDto.cs
+++ Fixit.Core.Database.DataContracts/Documents/DocumentCollectionDto.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using Fixit.Core.DataContracts;
 
 namespace Fixit.Core.Database.DataContracts.Documents
 {
+  [DataContract]
   public class DocumentCollectionDto<T> : OperationStatus
   {
     private IList<T> _results;
+
+    [DataMember]
     public IList<T> Results
     {
       get => _results ??= new List<T>();
